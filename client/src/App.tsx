c3d27f1ea409e8e347e7886ea63de4d02e2ad2e9
kushal198 <kushalkc198@gmail.com>
2022-06-27 10:55:02 +0545

    added-style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Box} from '@mui/material';
 import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
